f73193e37c12a39e3ae67d94347a0a5bffa799ea
Fogarty <sfogarty@volt001.slac.stanford.edu>
2023-07-14 19:59:17 -0700

    First implementation of LArNEST

--- larndsim/quenching.py
+++ larndsim/quenching.py
@@ -4,41 +4,58 @@ through the detector
 """
 
 from math import log, isnan
-from numba import cuda
-
+import larnestpy
+import numpy as np
 from .consts import detector, physics, light
 
-@cuda.jit
-def quench(tracks, mode):
+def quench(track, calc, tracks_dtype):
     """
-    This CUDA kernel takes as input an array of track segments and calculates
+    This function takes as input an array of track segments and calculates
     the number of electrons and photons that reach the anode plane after recombination.
     It is possible to pick among two models: Box (Baller, 2013 JINST 8 P08005) or
     Birks (Amoruso, et al NIM A 523 (2004) 275).
 
     Args:
-        tracks (:obj:`numpy.ndarray`): array containing the tracks segment information
-        mode (int): recombination model (physics.BOX or physics.BIRKS).
+        track (:obj:`numpy.ndarray`): array containing the track segment information
     """
-    itrk = cuda.grid(1)
-
-    if itrk < tracks.shape[0]:
-        dEdx = tracks[itrk]["dEdx"]
-        dE = tracks[itrk]["dE"]
-
-        recomb = 0
-        if mode == physics.BOX:
-            # Baller, 2013 JINST 8 P08005
-            csi = physics.BOX_BETA * dEdx / (detector.E_FIELD * detector.LAR_DENSITY)
-            recomb = max(0, log(physics.BOX_ALPHA + csi)/csi)
-        elif mode == physics.BIRKS:
-            # Amoruso, et al NIM A 523 (2004) 275
-            recomb = physics.BIRKS_Ab / (1 + physics.BIRKS_kb * dEdx / (detector.E_FIELD * detector.LAR_DENSITY))
-        else:
-            raise ValueError("Invalid recombination mode: must be 'physics.BOX' or 'physics.BIRKS'")
-
-        if isnan(recomb):
-            raise RuntimeError("Invalid recombination value")
-
-        tracks[itrk]["n_electrons"] = recomb * dE / physics.W_ION
-        tracks[itrk]["n_photons"] = (dE/light.W_PH - tracks[itrk]["n_electrons"]) * light.SCINT_PRESCALE
+    track = np.array(track, dtype=tracks_dtype)
+    dEdx = track['dEdx']
+    dE = track['dE']
+    dx = track['dx']
+    recomb = 0
+    pdg_id = track['pdgId']
+    E_cut = 1.0 # MeV
+    
+    if pdg_id == 11:
+        m_electron = 0.510999
+        p_start = np.sqrt(np.sum(track['traj_pxyz_start']**2))
+        E = np.sqrt(m_electron**2 + p_start**2) - m_electron
+        if E < E_cut:
+            # use LArNEST ER model
+            result = calc.full_calculation(larnestpy.LArInteraction.ER, E*1e3, dx, detector.E_FIELD*1e3, detector.LAR_DENSITY, False)
+            recomb = result.yields.Ne / (result.yields.Ne + result.yields.Nph)
+    elif pdg_id == 1000020040:
+        # use LArNEST alpha model
+        m_alpha = 3753.63
+        p_start = np.sqrt(np.sum(track['traj_pxyz_start']**2))
+        E = np.sqrt(m_alpha**2 + p_start**2) - m_alpha
+        result = calc.get_alpha_yields(E*1e3, detector.E_FIELD*1e3, detector.LAR_DENSITY)
+        recomb = result.Ne / (result.Ne + result.Nph)
+    elif pdg_id == 1000180400:
+        # use LArNEST NR model for recoiling Ar40
+        m_Ar40 = 37284.0
+        p_start = np.sqrt(np.sum(track['traj_pxyz_start']**2))
+        E = np.sqrt(m_Ar40**2 + p_start**2) - m_Ar40
+        result = calc.get_nr_yields(E*1e3, detector.E_FIELD*1e3, detector.LAR_DENSITY)
+        recomb = result.Ne / (result.Ne + result.Nph)
+    else:
+        # use LArNEST dE/dx model
+        result = calc.get_dEdx_recombination_probability(dEdx, detector.E_FIELD*1e3)
+        recomb = result
+    if isnan(recomb):
+        raise RuntimeError("Invalid recombination value")
+    
+    track['n_electrons'] = recomb * dE / physics.W_ION
+    track['n_photons'] = (dE/light.W_PH - track['n_electrons']) * light.SCINT_PRESCALE
+    
+    return track
